cdf93cebd504efb6353093348cd07d0ae1bc0ee6
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2025-01-21 23:56:40 -0500

    feat: work in progress

---
+++ docker_setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+echo "Setup script has started ..."
+apt update && apt upgrade -y
+apt install tree git curl vim zsh expect -y
+echo "Setup script ha s completed the installation!"
