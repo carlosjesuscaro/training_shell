bae0edc168056c31852e8a5f7575112bc361b75d
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2025-01-27 07:26:59 -0500

    feat: File couner completed

--- file_counter.sh
+++ file_counter.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/bash
 
 path=$1
-echo $path
+# echo $path
+ls_out=$(ls -a $path 2>&1) # Capturing the ls output as an array
+#echo $ls_out
+
+function ls_looping {
+  local -n arr=$1
+  for element in "${arr[@]}"; do
+    echo "$element"
+  done
+}
+
+regex=".*No such file or directory"
+if [[ $ls_out =~ $regex ]]; then
+  echo "Wrong path. Try again!"
+else
+  ls_out=( $(ls -a $path) )
+  echo "There are ${#ls_out[*]} files on $path"
+  ls_looping ls_out
+fi
